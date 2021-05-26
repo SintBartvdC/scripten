befaa0c6b23db18b76dcf3877ea5b3a800dbe180
SintBartvdC <b.van.de.crommert@sintlucasedu.nl>
2021-05-26 11:49:39 +0200

    classes.

---
+++ 1.2 classes/1.2 classes/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace _1._2_classes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            students bart = new students();
+
+            bart.SetName("bart");
+
+            bart.hobby = "rugby";
+
+            bart.birth = "28-05-2003";
+
+            bart.alive = "alive";
+
+            Console.WriteLine("the students name is {0}", bart.GetName());
+
+            Console.WriteLine("the student is born on {0}", bart.yearofbirth());
+
+            Console.WriteLine("hobby is {1}", bart.GetName(), bart.hobby);
+
+            Console.WriteLine("the student is {0}", bart.AliveOrNOt());
+
+
+        }
+
+    }
+
+}
---
+++ 1.2 classes/1.2 classes/students .cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _1._2_classes
+{
+    class students
+    {
+        private string name;
+
+        public void Birth()
+        {
+            Console.WriteLine("{0} birth {1}",
+                name, birth);
+        }
+
+        public students()
+        : this("No Name") { }
+
+        public students(string name)
+            : this(name, "no year of birth") { }
+
+
+        public students(string name, string birth)
+        {
+            SetName(name);
+        }
+
+
+        public void SetName(string name)
+        {
+            if (!name.Any(char.IsDigit))
+            {
+                this.name = name;
+            }
+            else
+            {
+                this.name = "No Name";
+                Console.WriteLine("Name can't contain numbers");
+            }
+        }
+
+        public string GetName()
+        {
+            return name;
+        }
+
+        public string hobby { get; set; } = "No hobby";
+
+        public string birth { get; set; } = "No birth";
+
+        public string alive { get; set; } = "not alive";
+
+        public string yearofbirth()
+        {
+            return birth;
+        }
+
+        public string AliveOrNOt()
+        {
+            return alive;
+        }
+    }
+}
